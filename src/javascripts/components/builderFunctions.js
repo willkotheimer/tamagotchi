dd8ef2e75aba04dff683bb0f37cfe4aec6e60746
wkotheimer <wkotheimer@gmail.com>
2020-09-05 12:56:10 -0500

    Added listeners

--- src/javascripts/components/builderFunctions.js
+++ src/javascripts/components/builderFunctions.js
@@ -1,3 +1,5 @@
+import Listener from './eventListeners';
+
 const printToDom = (divId, textToPrint) => {
   const selectedDiv = document.getElementById(divId);
   selectedDiv.innerHTML = textToPrint;
@@ -10,11 +12,13 @@ const domStringBuilder = (array) => {
     <div class="name">${tg.quadrantName}</div>
     <div class="score">${tg.quadrantTotal}</div>
     <div class="buttons">
-      <button data-value="${tg.button1Value}" id="${tg.quadrantName}-button-1">${tg.button1Name}</button>
-      <button data-value="${tg.button2Value}" id="${tg.quadrantName}-button-2" class="button-${tg.button2Name}">${tg.button2Name}</button>
+      <button data-value="${tg.button1Value}" id="${tg.quadrantName.toLowerCase()}-button-1">${tg.button1Name}</button>
+      <button data-value="${tg.button2Value}" id="${tg.quadrantName.toLowerCase()}-button-2" class="button-${tg.button2Name}">${tg.button2Name}</button>
     </div>
 </div>`;
+
     printToDom(`${tg.quadrantName.toLowerCase()}`, template);
+    Listener.addEventListener(`${tg.quadrantName.toLowerCase()}`);
   });
 };
 export default { domStringBuilder };
--- src/javascripts/components/eventListeners.js
+++ src/javascripts/components/eventListeners.js
@@ -1,4 +1,12 @@
-const buttonEventFunction = (e) => {
-  const buttonID = e.target.closest('button').id;
-  
-}; 
+const addEventListener = (name) => {
+  const button1 = $(`#${name}-button-1`);
+  const button2 = $(`#${name}-button-2`);
+  button1.on('click', (e) => {
+    console.warn(e.target.id);
+  });
+  button2.on('click', (e) => {
+    console.warn(e.target.id);
+  });
+};
+
+export default { addEventListener };
